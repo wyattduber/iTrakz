70d80ab81253f40204fd8d2b1a2a1be7fedcec90
Zachary Kline <thefuzzyfish2@gmail.com>
2021-03-25 19:42:19 -0500

    Converted database functions into a single database class

--- sqlite/db.js
+++ sqlite/db.js
@@ -1,269 +1,272 @@
 let db; // Initialize Database Variable
 
-function dbInit() {
-    try {
-        db = openDatabase('itrakz', '1.0', 'Tickets', 2 * 1024 * 1024);
-        db.transaction(function (tx) {
-            tx.executeSql(
-                'CREATE TABLE IF NOT EXISTS tickets(' +
-                'id INT NOT NULL, ' +
-                'title VARCHAR(200) NOT NULL, ' +
-                'author VARCHAR(50) NOT NULL, ' +
-                'time DATETIME NOT NULL, ' +
-                'content LONGTEXT NOT NULL, ' +
-                'label VARCHAR(20) NOT NULL, ' +
-                'projectTitle VARCHAR(50), ' +
-                'responder VARCHAR(50), ' +
-                'category VARCHAR(20)' +
-                ');'
-            );
+class database {
 
-            tx.executeSql(
-                'CREATE TABLE IF NOT EXISTS history(' +
-                'id INT NOT NULL, ' +
-                'date DATETIME NOT NULL, ' +
-                'description VARCHAR(200) NOT NULL, ' +
-                'user VARCHAR(50) NOT NULL' +
-                ');'
-            );
+    constructor() {
+        try {
+            db = openDatabase('itrakz', '1.0', 'Tickets', 2 * 1024 * 1024);
+            db.transaction(function (tx) {
+                tx.executeSql(
+                    'CREATE TABLE IF NOT EXISTS tickets(' +
+                    'id INT NOT NULL, ' +
+                    'title VARCHAR(200) NOT NULL, ' +
+                    'author VARCHAR(50) NOT NULL, ' +
+                    'time DATETIME NOT NULL, ' +
+                    'content LONGTEXT NOT NULL, ' +
+                    'label VARCHAR(20) NOT NULL, ' +
+                    'projectTitle VARCHAR(50), ' +
+                    'responder VARCHAR(50), ' +
+                    'category VARCHAR(20)' +
+                    ');'
+                );
 
-        });
-    } catch (e) {
-        console.error(e);
-    }
-}
+                tx.executeSql(
+                    'CREATE TABLE IF NOT EXISTS history(' +
+                    'id INT NOT NULL, ' +
+                    'date DATETIME NOT NULL, ' +
+                    'description VARCHAR(200) NOT NULL, ' +
+                    'user VARCHAR(50) NOT NULL' +
+                    ');'
+                );
 
-function getTicketBit(id, bit) {
-    try{
-        db.transaction(function(tx) {
-            switch (bit) {
-                case "title":
-                    tx.executeSql('SELECT title FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "author":
-                    tx.executeSql('SELECT author FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "time":
-                    tx.executeSql('SELECT time FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "content":
-                    tx.executeSql('SELECT content FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "label":
-                    tx.executeSql('SELECT label FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "projectName":
-                    tx.executeSql('SELECT projectName FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "responder":
-                    tx.executeSql('SELECT responder FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "category":
-                    tx.executeSql('SELECT name FROM tickets WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                default:
-                    throw new SQLException();
-            }
-        });
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function getHistoryBit(id, bit) {
-    try {
-        db.transaction(function(tx) {
-            switch (bit) {
-                case "date":
-                    tx.executeSql('SELECT date FROM history WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "description":
-                    tx.executeSql('SELECT description FROM history WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                case "user":
-                    tx.executeSql('SELECT user FROM history WHERE id=?', [id], function (tx, resultSet) {
-                        return resultSet.rows.item(0);
-                    });
-                    break;
-                default:
-                    throw new SQLException();
-            }
-        });
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function getTicketById(id) {
-    let i, arr;
-    try {
-        db.transaction(function(tx) {
-           tx.executeSql('SELECT * FROM tickets WHERE id=?', [id], function(tx, results) {
-               arr = [results.rows.length];
-               for (i = 0; i < results.rows.length; i++) {
-                   arr[i] = results.rows.item(i);
-               }
-               return arr;
-           });
-        });
-
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function getTicketsByAuthor(author) {
-    try {
-        db.transaction(function(tx) {
-            tx.executeSql('SELECT id FROM tickets WHERE author=?', [author], function(tx, results) {
-                let i, arr = [results.rows.length];
-                for (i = 0; i < results.rows.length; i++) {
-                    arr[i] = results.rows.item(i);
-                }
-                return arr;
             });
-        })
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function getTicketsByLabel(label) {
-    try {
-        db.transaction(function(tx) {
-           tx.executeSql('SELECT id FROM tickets WHERE label=?', [label], function(tx, results) {
-               let length = results.rows.length, i, arr = [results.rows.length];
-               for (i = 0; i < length; i++) {
-                   arr[i] = results.rows.item(i);
-               }
-               return arr;
-           });
-        });
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function updateProjectTitle(newProjectTitle, id, date, user) {
-    try {
-        db.transaction(function (tx) {
-            tx.executeSql('UPDATE tickets SET projectTitle=? WHERE id=?', [newProjectTitle, id]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    createHistory(date, "Project Update - " + newProjectTitle, user);
-}
-
-function updateResponder(responder, id, date, user) {
-    try {
-        db.transaction(function(tx) {
-            tx.executeSql('UPDATE tickets SET responder=? WHERE id=?', [responder, id]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+    *getTicketBit(id, bit) {
+        try {
+            db.transaction(function (tx) {
+                switch (bit) {
+                    case "title":
+                        tx.executeSql('SELECT title FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "author":
+                        tx.executeSql('SELECT author FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "time":
+                        tx.executeSql('SELECT time FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "content":
+                        tx.executeSql('SELECT content FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "label":
+                        tx.executeSql('SELECT label FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "projectName":
+                        tx.executeSql('SELECT projectName FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "responder":
+                        tx.executeSql('SELECT responder FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "category":
+                        tx.executeSql('SELECT name FROM tickets WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    default:
+                        throw new SQLException();
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    createHistory(date, "Ticket Response", user);
-}
-
-function updateCategory(newCategory, id, date, user) {
-    try {
-        db.transaction(function (tx) {
-            tx.executeSql('UPDATE tickets SET category=? WHERE id=?', [newCategory, id]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+    *getHistoryBit(id, bit) {
+        try {
+            db.transaction(function (tx) {
+                switch (bit) {
+                    case "date":
+                        tx.executeSql('SELECT date FROM history WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "description":
+                        tx.executeSql('SELECT description FROM history WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    case "user":
+                        tx.executeSql('SELECT user FROM history WHERE id=?', [id], function (tx, resultSet) {
+                            return resultSet.rows.item(0);
+                        });
+                        break;
+                    default:
+                        throw new SQLException();
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    createHistory(date, "Category Update - " + newCategory, user);
-}
+    *getTicketById(id) {
+        let i, arr;
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('SELECT * FROM tickets WHERE id=?', [id], function (tx, results) {
+                    arr = [results.rows.length];
+                    for (i = 0; i < results.rows.length; i++) {
+                        arr[i] = results.rows.item(i);
+                    }
+                    return arr;
+                });
+            });
 
-function updateLabel(newLabel, id, date, user) {
-    try {
-        db.transaction(function(tx) {
-            tx.executeSql('UPDATE tickets SET label=? WHERE id=?', [newLabel, id]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    createHistory(date, "Label Change - " + newLabel, user);
-}
-
-function updateContent(newContent, id, date, user) {
-    try {
-        db.transaction(function(tx) {
-            tx.executeSql('UPDATE tickets SET content=? WHERE id=?', [newContent, id]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+    *getTicketsByAuthor(author) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('SELECT id FROM tickets WHERE author=?', [author], function (tx, results) {
+                    let i, arr = [results.rows.length];
+                    for (i = 0; i < results.rows.length; i++) {
+                        arr[i] = results.rows.item(i);
+                    }
+                    return arr;
+                });
+            })
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    createHistory(date, "Content Edit", user);
-}
-
-function updateTitle(newTitle, id, date, user) {
-    try {
-        db.transaction(function(tx) {
-            tx.executeSql('UPDATE tickets SET title=? WHERE id=?', [newTitle, id]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+    *getTicketsByLabel(label) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('SELECT id FROM tickets WHERE label=?', [label], function (tx, results) {
+                    let length = results.rows.length, i, arr = [results.rows.length];
+                    for (i = 0; i < length; i++) {
+                        arr[i] = results.rows.item(i);
+                    }
+                    return arr;
+                });
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
 
-    createHistory(date, "Title Change - " + newTitle, user);
-}
+    *updateProjectTitle(newProjectTitle, id, date, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('UPDATE tickets SET projectTitle=? WHERE id=?', [newProjectTitle, id]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
 
-function createTicket(id, title, author, time, content, label, projectTitle, responder, category) {
-    try {
-        db.transaction(function (tx) {
-            tx.executeSql('INSERT INTO tickets(id,title,author,time,content,label,projectTitle,responder,category) VALUES (?,?,?,?,?,?,?,?,?)',
-                [id, title, author, time, content, label, projectTitle, responder, category]);
-        });
-    } catch (e) {
-        console.error(e);
-        return;
+        createHistory(date, "Project Update - " + newProjectTitle, user);
     }
 
-    createHistory(time, "Opened Ticket", author);
-}
+    *updateResponder(responder, id, date, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('UPDATE tickets SET responder=? WHERE id=?', [responder, id]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
 
-function createHistory(date, description, user) {
-    try {
-        db.transaction(function(tx) {
-            tx.executeSql('INSERT INTO history(date,description,user) VALUES (?,?,?)',
-                [date, description, user]);
-        });
-    } catch (e) {
-        console.error(e);
+        createHistory(date, "Ticket Response", user);
+    }
+
+    *updateCategory(newCategory, id, date, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('UPDATE tickets SET category=? WHERE id=?', [newCategory, id]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+
+        createHistory(date, "Category Update - " + newCategory, user);
+    }
+
+    *updateLabel(newLabel, id, date, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('UPDATE tickets SET label=? WHERE id=?', [newLabel, id]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+
+        createHistory(date, "Label Change - " + newLabel, user);
+    }
+
+    *updateContent(newContent, id, date, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('UPDATE tickets SET content=? WHERE id=?', [newContent, id]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+
+        createHistory(date, "Content Edit", user);
+    }
+
+    *updateTitle(newTitle, id, date, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('UPDATE tickets SET title=? WHERE id=?', [newTitle, id]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+
+        createHistory(date, "Title Change - " + newTitle, user);
+    }
+
+    *createTicket(id, title, author, time, content, label, projectTitle, responder, category) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('INSERT INTO tickets(id,title,author,time,content,label,projectTitle,responder,category) VALUES (?,?,?,?,?,?,?,?,?)',
+                    [id, title, author, time, content, label, projectTitle, responder, category]);
+            });
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+
+        createHistory(time, "Opened Ticket", author);
+    }
+
+    *createHistory(date, description, user) {
+        try {
+            db.transaction(function (tx) {
+                tx.executeSql('INSERT INTO history(date,description,user) VALUES (?,?,?)',
+                    [date, description, user]);
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
 
