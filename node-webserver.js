8cdc31de7f544ded9ae031ff53f8b857cc1f3667
Zachary Kline <thefuzzyfish2@gmail.com>
2021-03-25 21:05:39 -0500

    Switched server request handler back to default from dbTest

--- node-webserver.js
+++ node-webserver.js
@@ -3,7 +3,7 @@ const http = require('http'); // Node HTTP handler
 const handlers = require('./requestHandlers.js'); // Imports functions from requestHandlers.js
 
 /* Start server */
-var server = http.createServer(handlers.dbTest); // Sends incoming connections to requestHandlers.js
+var server = http.createServer(handlers.default); // Sends incoming connections to requestHandlers.js
 const port = 8080; // default port is 80, but setting to secondary standard 8080. Prod systems should be set to 80
 server.listen(port); // Starts server
 
